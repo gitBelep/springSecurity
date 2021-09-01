c21c6c3d769d6bd70d3c1481c33a0dfae2e4120b
gitBelep <ddawid3@freemail.hu>
2021-09-01 22:13:21 +0200

    v1 - works with memory

--- src/main/java/springSecurity/SecurityConfig.java
+++ src/main/java/springSecurity/SecurityConfig.java
@@ -2,16 +2,16 @@ package springSecurity;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import javax.sql.DataSource;
 
+@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private DataSource ds;
 
     @Bean
     public PasswordEncoder passwordEncoder(){
@@ -21,12 +21,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
-                .jdbcAuthentication()
-                .dataSource(ds);
-//                .inMemoryAuthentication()
-//                .withUser("user1")
-//                .password("$2a$10$3E6Zz9F47tgR/gvlNNNO/e6T21/rxA9sPF/I8vGK0Sx4HbY8XEZQ.")
-//                .authorities("normal_user");
+                .inMemoryAuthentication()
+                .withUser("user1")
+                .password("$2a$10$3E6Zz9F47tgR/gvlNNNO/e6T21/rxA9sPF/I8vGK0Sx4HbY8XEZQ.")
+                .authorities("normal_user");
     }
 
 }
